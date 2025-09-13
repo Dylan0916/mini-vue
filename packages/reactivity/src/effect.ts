97403d0a99a39ba09df75253790ef40aac8c91b8
Dylan <klj40702@gmail.com>
2025-09-13 11:26:44 +0800

    day 3

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,3 +1,7 @@
+export let activeSub = null
+
 export function effect(fn) {
+  activeSub = fn
   fn()
+  activeSub = null
 }
--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,19 +1,40 @@
+import { activeSub } from './effect'
+
+enum ReactiveFlags {
+  IS_REF = '__v_isRef',
+}
+
 class RefImpl {
+  [ReactiveFlags.IS_REF] = true
   _value
+  _sub = new Set()
 
   constructor(value) {
     this._value = value
   }
 
   get value() {
+    if (activeSub) {
+      this._sub.add(activeSub)
+    }
+
     return this._value
   }
 
   set value(newVal) {
     this._value = newVal
+    this._sub.forEach(sub => {
+      if (typeof sub === 'function') {
+        sub()
+      }
+    })
   }
 }
 
 export function ref(value) {
   return new RefImpl(value)
+}
+
+export function isRef(value) {
+  return !!(value && value[ReactiveFlags.IS_REF])
 }
